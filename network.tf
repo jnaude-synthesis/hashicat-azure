604d7058cd7d00b2720df59c862456ab31ce1a4c
root <jared@synthesis.co.za>
2022-03-24 13:53:10 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -2,6 +2,6 @@ module "network" {
   source  = "app.terraform.io/syntraining/network/azurerm"
   version = "3.5.0"
 
-  resource_group_name = azurerm_resource_group.myresourcegroup.resource_group_name
+  resource_group_name = "${var.prefix}-workshop"
   # insert required variables here
 }
